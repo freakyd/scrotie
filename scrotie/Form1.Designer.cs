ef2009aaafd821b172fe2a6a0761e5a33664d036
freakyd <iamabigwiener666@hotmail.com>
2015-05-04 17:16:38 -0700

    SpaCe engIneeRs save gamE ediTOr(SCROTIE)
    This program edits your space engineers save game. It was a learning
    project for XML, but it's also great at exploring your save game. I
    didn't draw that optimus prime logo, it was free for personal use.

---
+++ scrotie/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace scrotie
+{
+    partial class SEedit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SEedit));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.RemoveButton);
+            this.panel2.Controls.Add(this.QuitButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 440);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(635, 46);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Components: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(226, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Objects: 0";
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(113, 6);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(107, 34);
+            this.RemoveButton.TabIndex = 2;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButtonClick);
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(3, 6);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(104, 34);
+            this.QuitButton.TabIndex = 1;
+            this.QuitButton.Text = "Quit";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButtonClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(635, 440);
+            this.panel1.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView2);
+            this.splitContainer1.Size = new System.Drawing.Size(635, 440);
+            this.splitContainer1.SplitterDistance = 349;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(349, 440);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(282, 440);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // SEedit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 486);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SEedit";
+            this.Text = "XML Editor";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ scrotie/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scrotie
+{
+    
+    public partial class SEedit : Form
+    {
+        XmlRead DataC;
+        public SEedit()
+        {
+            InitializeComponent();
+            
+            DataC = new XmlRead("SANDBOX_0_0_0_.sbs");
+            DataC.ShowSector();
+            dataGridView1.DataSource = DataC.Ships;
+            label1.Text = "Objects: " + DataC.Ships.Rows.Count.ToString();
+        }
+
+        private void QuitButtonClick(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void RemoveButtonClick(object source, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if (dataGridView1.SelectedRows[0].Cells[0].Value != null)
+                {
+                    var Result = MessageBox.Show("Delete " + dataGridView1.SelectedRows[0].Cells[1].Value.ToString() + "?",
+                        "Confirm Delete!", MessageBoxButtons.YesNo);
+                    if (Result == DialogResult.Yes)
+                    {
+                        int count = 0;
+                        foreach (DataGridViewRow Row in dataGridView1.SelectedRows)
+                        {
+                            DataC.RemoveObject(Row.Cells[0].Value.ToString());
+                            var Squery = DataC.Ships.AsEnumerable().Where(r => r.Field<string>("ID") == Row.Cells[0].Value.ToString());
+                            var row = Squery.ToList();
+                            row[0].Delete();
+                            count++;
+                        }
+                        MessageBox.Show(count + "Object(s) Removed");
+                        dataGridView1.DataSource = DataC.Ships;
+                        label1.Text = "Objects: " + DataC.Ships.Rows.Count.ToString();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select a Row");
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int Row = dataGridView1.Rows.Count - 1;
+            try { Row = dataGridView1.SelectedCells[0].RowIndex; }
+            catch { Row--; }
+            if (Row >= 0)
+            {
+                dataGridView1.Rows[Row].Selected = true;
+                if (dataGridView1.SelectedRows[0].Cells[0].Value != null)
+                {
+                    DataC.ShowObject(dataGridView1.SelectedRows[0].Cells[0].Value.ToString());
+                    dataGridView2.DataSource = DataC.Components;
+                    label2.Text = "Components: " + DataC.Components.Rows.Count.ToString();
+                }
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Control.ModifierKeys == Keys.Control && e.KeyCode == Keys.F)
+            {
+                FindDialog FindString = new FindDialog();
+                if (FindString.ShowDialog() == DialogResult.OK)
+                {
+                    string FString = FindString.textBox1.Text;
+                    DataGridViewRow row = null;
+                    try
+                    {
+                        row = dataGridView1.Rows
+                            .Cast<DataGridViewRow>()
+                            .Where(r => r.Cells[0].Value.ToString().Contains(FString)).First();
+                    }
+                    catch { }
+                    if (row == null)
+                    {
+                        try
+                        {
+                            row = dataGridView1.Rows
+                                .Cast<DataGridViewRow>()
+                                .Where(r => r.Cells[1].Value.ToString().Contains(FString)).First();
+                        }
+                        catch { }
+                    }
+
+                    if (row != null)
+                    {
+                        dataGridView1.CurrentCell = dataGridView1[0, row.Index];
+                        dataGridView1.Rows[row.Index].Selected = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show(FString + " not Found");
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int Row = dataGridView1.Rows.Count - 1;
+            try { Row = dataGridView1.SelectedCells[0].RowIndex; }
+            catch { Row--; }
+            if (Row >= 0)
+            {
+                dataGridView1.Rows[Row].Selected = true;
+                if (dataGridView1.SelectedRows[0].Cells[0].Value != null)
+                {
+                    DataC.ShowObject(dataGridView1.SelectedRows[0].Cells[0].Value.ToString());
+                    dataGridView2.DataSource = DataC.Components;
+                    label2.Text = "Components: " + DataC.Components.Rows.Count.ToString();
+                }
+            }
+        }
+
+    }
+    public class XmlRead
+    {
+        string FileName = "SANDBOX_0_0_0_.sbs";
+        XDocument xmlDoc;
+        static DataTable ComponentTable = new DataTable();
+        static DataTable ShipTable = new DataTable();
+        
+        public XmlRead(string File)
+        {
+            if (File.Length > 0) FileName = File;
+            try { xmlDoc = XDocument.Load(FileName); }
+            catch
+            {
+                var SelectFile = new OpenFileDialog();
+                if (SelectFile.ShowDialog() == DialogResult.OK)
+                {
+                    FileName = SelectFile.FileName;
+                    try { xmlDoc = XDocument.Load(FileName); }
+                    catch { MessageBox.Show("Unable to open " + FileName); Environment.Exit(1); }
+                }
+                else
+                {
+                    MessageBox.Show("Unable to open " + FileName);
+                    Environment.Exit(1);
+                }
+            }
+        }
+        public string truncate(string input, char letter)
+        {
+            int Index = input.IndexOf(letter);
+            if (Index != -1) return input.Substring(0, Index);
+            else return input;
+        }
+        public void ShowObject(string objID)
+        {
+            if (ComponentTable.Columns.Count == 0)
+            {
+                ComponentTable.Columns.Add("Type", typeof(string));
+                ComponentTable.Columns.Add("SubtypeName", typeof(string));
+                ComponentTable.Columns.Add("CustomName", typeof(string));
+            }
+            ComponentTable.Clear();
+            List<XElement> items = (from el in xmlDoc.Descendants("MyObjectBuilder_EntityBase")
+                                    where
+                                    (
+                                        (string)el.Element("EntityId") == objID
+                                    )
+                                    select el).ToList<XElement>();
+            XElement Comp = items[0];
+            if (Comp.Element("Item") != null) //items
+            {
+                ComponentTable.Columns.RemoveAt(2);
+                ComponentTable.Columns.Add("Amount", typeof(string));
+                string TypeS = Comp.Element("Item").Element("PhysicalContent").FirstAttribute.Value;
+                ComponentTable.Rows.Add(TypeS, Comp.Element("Item").Element("PhysicalContent").Element("SubtypeName").Value, Comp.Element("Item").Element("Amount").Value);
+            }
+            else if (Comp.Element("StorageName") != null) //asteroid
+            {
+            }
+            else
+            {//base/ship
+                IEnumerable<XElement> Blocks = Comp.Descendants("MyObjectBuilder_CubeBlock");
+                ComponentTable.Columns.RemoveAt(2);
+                ComponentTable.Columns.Add("CustomName", typeof(string));
+                foreach (XElement el in Blocks)
+                {
+                    string TypeS = "", SubtypeName = "" , CustomName = "";
+                    TypeS = el.FirstAttribute.Value.ToString();
+                    if (el.Element("SubtypeName") != null) SubtypeName = el.Element("SubtypeName").Value.ToString();
+                    if (el.Element("CustomName") != null) CustomName = el.Element("CustomName").Value.ToString();
+                    ComponentTable.Rows.Add(TypeS, SubtypeName, CustomName);
+                }
+            }
+
+        }
+        public void ShowSector()
+        {
+            ShipTable.Columns.Add("ID", typeof(string));
+            ShipTable.Columns.Add("Ship", typeof(string));
+            ShipTable.Columns.Add("X", typeof(string));
+            ShipTable.Columns.Add("Y", typeof(string));
+            ShipTable.Columns.Add("Z", typeof(string));
+
+            IEnumerable<XElement> items = xmlDoc.Descendants("MyObjectBuilder_EntityBase");
+            foreach (XElement e in items)
+            {
+                if (e.Element("DisplayName") == null)//not a ship
+                {
+                    if (e.Element("StorageName") == null) //not an asteroid
+                    {
+                        if (e.Element("Item") == null)//not an item
+                        {
+                            //Console.WriteLine(e);//unknown..still a mystery, so do nothing.
+                        }
+                        else
+                        {
+                            ShipTable.Rows.Add(e.Element("EntityId").Value,
+                                e.Element("Item").Element("PhysicalContent").Element("SubtypeName").Value,
+                                truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("x").Value, '.'),
+                                truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("y").Value, '.'),
+                                truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("z").Value, '.'));
+                        }
+
+                    }
+                    else //do nothing to asteroids
+                    {
+                        /*ShipTable.Rows.Add(e.Element("EntityId").Value,
+                        e.Element("StorageName").Value,
+                        truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("x").Value,'.'),
+                        truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("y").Value,'.'),
+                        truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("z").Value,'.'));*/
+                    }
+                }
+                else
+                {
+                    ShipTable.Rows.Add(e.Element("EntityId").Value,
+                                        e.Element("DisplayName").Value,
+                        truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("x").Value, '.'),
+                        truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("y").Value, '.'),
+                        truncate(e.Element("PositionAndOrientation").Element("Position").Attribute("z").Value, '.'));
+                }
+            }
+        }
+        public DataTable Ships
+        {
+            get
+            {
+                return ShipTable;
+            }
+        }
+        public DataTable Components
+        {
+            get
+            {
+                return ComponentTable;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return ShipTable.Rows.Count;
+            }
+        }
+        public void RemoveObject(string objID)
+        {
+            xmlDoc.Descendants("MyObjectBuilder_EntityBase").Where(xe => xe.Element("EntityId").Value == objID).Single().Remove();
+            xmlDoc.Save(FileName);
+        }
+    }
+}
